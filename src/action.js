078888bc713250bbb5f2afdc7747b0841bc73883
Sabin Subedi <sabinsubedi125@gmail.com>
2022-09-27 14:53:18 +0545

    added the thank you commenting

--- src/action.js
+++ src/action.js
@@ -1,5 +1,17 @@
-async function run() {
-  console.log("Hello, world!");
-}
+const core = require("@actions/core");
+const github = require("@actions/github");
 
-run();
+const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
+
+const octokit = github.getOctokit(GITHUB_TOKEN);
+
+const { context = {} } = github;
+const { pull_request } = context.payload;
+
+(async function () {
+  await octokit.issues.createComment({
+    ...context.repo,
+    issue_number: pull_request.number,
+    body: "Thank you for submitting a pull request! We will try to review this as soon as we can.",
+  });
+})();
